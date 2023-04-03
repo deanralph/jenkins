42a2b2fc71999636f643be0a9ab602e8e9c1c0c9
Dean Ralph <dean.ralph@gmail.com>
2023-04-03 12:09:19 +0000

    modified:   addToPatching.py

--- addToPatching.py
+++ addToPatching.py
@@ -10,7 +10,7 @@ with open('/jenkins/sqlcreds.json') as f:
 conn = pymssql.connect(server=servcreds['server'], database=servcreds["database"], user=servcreds["username"], password=servcreds['password'])
 
 # Define SQL query
-sql = f"INSERT INTO [dbo].[servers] ([servername] ,[ipaddress] ,[patchingstatus] ,[rebootreq]) VALUES ({sys.argv[1]},> , {sys.argv[2]} ,'Unpatched' ,'False')"
+sql = f"INSERT INTO [dbo].[servers] ([servername] ,[ipaddress] ,[patchingstatus] ,[rebootreq]) VALUES ({sys.argv[1]}, {sys.argv[2]} ,'Unpatched' ,'False')"
 
 with conn.cursor() as cursor:
     cursor.execute(sql)
