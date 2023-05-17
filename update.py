0186f17ba54d399b0a953d03e904de2b760c6150
Dean Ralph <dean.ralph@gmail.com>
2023-05-17 16:43:24 +0000

    updated update

--- update.py
+++ update.py
@@ -14,9 +14,7 @@ conn = pymssql.connect(server=servcreds['server'], database=servcreds["database"
 sql = "SELECT servername, ipaddress FROM servers where patchingstatus = 'Unpatched'"
 
 def ping(host):
-    """
-    Returns True if host responds to a ping request, False otherwise.
-    """
+    # Returns True if host responds to a ping request, False otherwise.
     command = ['ping', '-c', '1', host]
     # Use subprocess.PIPE to redirect the output to the Python script
     # instead of printing it to the console
@@ -37,7 +35,7 @@ with conn.cursor() as cursor:
             
             # Connect to server using Fabric and run apt update
             with Connection(ipaddress) as c:
-                c.sudo('sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y && sudo apt-get autoclean -y')
+                c.sudo('sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y')
                 
         else:
             print("Server Offline")
